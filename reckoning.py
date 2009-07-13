e5b57d349fe6e0e99f7b5f94da8c4b724468225b
Will McCutchen <mccutchen@gmail.com>
2009-07-13 12:51:32 -0500

    Sign up for one job at a time by correctly manipulating the dumb ASP.NET postback machinery.

--- reckoning.py
+++ reckoning.py
@@ -1,4 +1,4 @@
-import datetime, hashlib, logging, random, sys, time
+import datetime, hashlib, logging, random, re, sys, time
 import mechanize
 
 QUEUE_URL = 'https://appraisalzone.lendervend.com/SECURE/plus/pfac/QueuePage.aspx'
@@ -14,15 +14,18 @@ USER_AGENT = 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)'
 
 EMPTY_CHECKSUM = '256f163a93109a1b99b178adeeb7d209'
 
-JOB_LIMIT = 5
+JOB_LIMIT = 3
 JOB_COUNT = 0
+
+JOB_ID_RE = r"\('([\w$]+)','(\d+)'\)"
 
 def get_browser():
 	browser = mechanize.Browser()
 
 	# I'm not sure which of these are required
 	browser.addheaders = [
-		('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.11) Gecko/2009060214 Firefox/3.0.11'),
+		('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X '
+		 '10.5; en-US; rv:1.9.0.11) Gecko/2009060214 Firefox/3.0.11'),
 		('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
 		('Accept-Language', 'en-us,en;q=0.5'),
 		('Accept-Encoding', 'gzip,deflate'),
@@ -64,34 +67,67 @@ def check_queue(browser):
 	browser.select_form(nr=0)
 
 	if filter(job_filter, browser.form.controls):
-		return get_jobs(browser)
+		return get_job(browser)
 	else:
 		print ' - Found no jobs...'
 
 	return browser
 
-def get_jobs(browser):
-	job_controls = filter(job_filter, browser.form.controls)
-	if len(job_controls) + JOB_COUNT >= JOB_LIMIT:
-		job_controls = job_controls[:JOB_LIMIT-JOB_COUNT]
+def get_job_id(control):
+	assert len(control.items) == 2, \
+		'Expected 2 items in the radio control, got %d' % len(control.items)
+	item = control.items[-1]
 
-	print ' * Getting %d job(s)...' % len(job_controls)
-	for control in job_controls:
-		control.value = ['1']
+	assert 'onclick' in item.attrs, \
+		'No onclick handler found for item %s' % item.id
+	onclick = item.attrs['onclick']
 
-	browser.submit()
+	match = re.search(JOB_ID_RE, onclick)
+	assert match is not None, \
+		'No job ID found in onclick event: %s' % onclick
 
-	filename = 'response-%s.html' % time.strftime('%Y%m%d-%H%M%S')
-	print ' * Writing response to %s...' % filename
-	file(filename, 'w').write(browser.response.read())
+	return match.groups()
 
-	JOB_COUNT += len(job_controls)
+def get_job(browser):
+	global JOB_LIMIT, JOB_COUNT
 	if JOB_COUNT >= JOB_LIMIT:
 		print ' * Reached job limit for this session.'
 		print ' * Exiting.'
 		sys.exit()
 
-	return browser
+	timestamp = time.strftime('%Y%m%d-%H%M%S')
+
+	filename = 'queue-%s.html' % timestamp
+	print ' * Writing queue response to %s...' % filename
+	file(filename, 'w').write(browser.response().read())
+
+	job_controls = filter(job_filter, browser.form.controls)
+	job_control = job_controls[0]
+	control_name, job_id = get_job_id(job_control)
+
+	# Set the appropriate values on the form
+	job_control.value = ['1']
+
+	# Dumb ASP.NET postback stuff
+	c = browser.form.find_control('__EVENTTARGET')
+	c.readonly = False
+	c.value = control_name
+
+	c = browser.form.find_control('__EVENTARGUMENT')
+	c.readonly = False
+	c.value = job_id
+
+	print ' * Getting job id %s...' % job_id
+	browser.submit()
+	JOB_COUNT += 1
+
+	filename = 'response-%s.html' % timestamp
+	print ' * Writing post-submit response to %s...' % filename
+	file(filename, 'w').write(browser.response().read())
+
+	# Check the queue again, to see if there are any more jobs
+	# available
+	return check_queue(browser)
 
 def main():
 	start = datetime.datetime.now()
@@ -101,13 +137,7 @@ def main():
 	login(browser)
 
 	while 1:
-		try:
-			check_queue(browser)
-		except Exception, e:
-			print ' * Caught exception: %s' % e
-			#print ' * Restarting...'
-			#login(browser)
-			#check_queue(browser)
+		check_queue(browser)
 
 		delay = random.randint(10, 60)
 		print 'Sleeping for %d seconds...\n' % delay
